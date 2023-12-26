f4b9a5ba1a89bfec9c67d2b6fd9345510405c8a7
Mubashir Tanwar <tanwar0210@gmail.com>
2023-12-26 22:46:10 +0530

    added utils

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser"
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"))
+app.use(cookieParser())
+ 
+export {app} 
