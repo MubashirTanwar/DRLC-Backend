afaa14ece0a979cb8aebf0cfd10f6a8fc74a9d77
Mubashir Tanwar <tanwar0210@gmail.com>
2023-12-31 04:09:56 +0530

    add request controllers

---
+++ src/controllers/request.controllers.js
@@ -0,0 +1,64 @@
+import { Request } from "../models/request.models.js";
+import { ApiError } from "../utils/apiError.js";
+import { ApiResponse } from "../utils/apiResponse";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const newRequest = asyncHandler( async (res, req) => {
+    const student = req.student
+    console.log(student);
+    // Files = parents_Dec, students_Dec, faculty_Rec, pdc
+    const { purpose, duration, ews, family_status } = req.body
+
+    if (
+        [purpose, duration, ews, family_status].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const existingReq = await Request.findOne({ // C O U L D   B E   B E T T E R
+        student_id: student._id    
+    })
+    console.log(existingReq);
+
+    if (existingReq.status !== ( "Fulfiled" || "Rejected" )){
+        throw new ApiError(402, "You have already applied be patient")
+    }
+
+    const parents_DecLocalPath = req.files?.parents_Dec[0]?.path;
+    const students_DecLocalPath = req.files?.students_Dec[0]?.path;
+    const faculty_RecLocalPath = req.files?.faculty_Rec[0]?.path;
+    const pdcLocalPath = req.files?.pdc[0]?.path;
+    if(!(parents_DecLocalPath || students_DecLocalPath || faculty_RecLocalPath || pdc)){
+        throw new ApiError(401, "All files are required")
+    }
+
+    const request = await Request.create({
+        purpose, 
+        duration, 
+        ews, 
+        family_status,
+        student_id: student._id,
+        parents_Dec: parents_DecLocalPath,
+        students_Dec: students_DecLocalPath,
+        faculty_Rec: faculty_RecLocalPath,
+        pdc: pdcLocalPath
+    }
+    )
+
+    const createdRequest = await Request.findById(request._id)  
+    if(!createdRequest) {
+        throw new ApiError("Error submitting Request")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, createdRequest, "Request Successful")
+    )
+
+})
+
+
+export {
+    newRequest
+}
