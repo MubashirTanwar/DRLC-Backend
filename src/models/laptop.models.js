84ae51164a67d77dd43294790f2c2ab113564a1d
Mubashir Tanwar <tanwar0210@gmail.com>
2024-01-01 22:35:53 +0530

    add laptop schema

---
+++ src/models/laptop.models.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const laptopSchema = new Schema( {
+    laptop_id: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    status: {
+        type: String,
+        required: true,
+        trim: true,
+        enum: ["Issued", "Check-Up", "Free"]
+    },
+    conditon: {
+        type: String,
+        required: true,
+    },
+    department: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true})
+
+export const Laptop = model("Laptop", laptopSchema)
