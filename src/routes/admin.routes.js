d73a4f391fce3b8526bea4feb3c350458c5450c0
Mubashir Tanwar <tanwar0210@gmail.com>
2024-01-24 23:54:50 +0530

    update admin routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,26 +1,47 @@
 import Router from "express";
-import { loginAdmin, logoutAdmin, registerAdmin, getRequests, getOneRequest, getApproved, getRequestsFromDepartment, updateRequest, viewProfile } from "../controllers/admin.controllers.js";
+import {
+    registerAdmin,
+    loginAdmin,
+    logoutAdmin,
+    getRequests,
+    getOneRequest,
+    getRequestsFromDepartment,
+    updateRequest,
+    viewProfile,
+    getOneApprovedRequest,
+} from "../controllers/admin.controllers.js";
+import {
+    getApproved,
+    newIssue,
+    allFreeLaptops,
+    allIssuedLaptops,
+    newReturn,
+    allReturn,
+} from "../controllers/issue.controllers.js";
 import { adminJWT } from "../middlewares/auth.middleware.js";
-import { newIssue } from "../controllers/issue.controllers.js";
 
-const adminRouter = Router()
+const adminRouter = Router();
 
-adminRouter.route("/register").post(registerAdmin)
-adminRouter.route("/login").post(loginAdmin)
+adminRouter.route("/register").post(registerAdmin);
+adminRouter.route("/login").post(loginAdmin);
 
 // R O U T E S
-adminRouter.route("/logout").post(adminJWT, logoutAdmin)
-adminRouter.route("/profile").get(adminJWT, viewProfile)
-adminRouter.route("/allRequest").get(adminJWT, getRequests)
-adminRouter.route("/deptRequests").get(adminJWT, getRequestsFromDepartment)
-adminRouter.route("/request/:request").get(adminJWT, getOneRequest)
-adminRouter.route("/update").patch(adminJWT, updateRequest) // TO DO
-
-
-
+adminRouter.route("/logout").post(adminJWT, logoutAdmin);
+adminRouter.route("/allRequest").get(adminJWT, getRequests);
+adminRouter.route("/deptRequests").get(adminJWT, getRequestsFromDepartment);
+adminRouter.route("/request/:request").get(adminJWT, getOneRequest);
+adminRouter.route("/profile").get(adminJWT, viewProfile);
+adminRouter.route("/update-request/:request").post(adminJWT, updateRequest);
 
 // M A I N T A I N A N C E
-adminRouter.route("/allApproved").get(adminJWT, getApproved)
-adminRouter.route("/newIssue").post(adminJWT, newIssue)
+adminRouter.route("/all-approved").get(adminJWT, getApproved);
+adminRouter
+    .route("/approved-request/:request")
+    .get(adminJWT, getOneApprovedRequest);
+adminRouter.route("/new-issue").post(adminJWT, newIssue);
+adminRouter.route("/free-laptop").get(adminJWT, allFreeLaptops);
+adminRouter.route("/issued-laptop").get(adminJWT, allIssuedLaptops);
+adminRouter.route("/new-return").post(adminJWT, newReturn);
+adminRouter.route("/all-return").get(adminJWT, allReturn);
 
-export { adminRouter }
+export { adminRouter };
